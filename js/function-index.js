dc44109e775cb663b91a86666ec62c26d418d253
Brenno Fagundes <dev.whiteflag@gmail.com>
2019-11-16 21:30:10 -0300

    Adicionado Fullcalendar

---
+++ js/function-index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: ['dayGrid', 'googleCalendar'],
+        googleCalendarApiKey: 'AIzaSyDRCvZHyGCiPZMpM1RUNO4WFj5XN71hduE',
+        eventSources: [
+            {
+                googleCalendarId: 'n7qhk1t35edo7m9p1gqgfl8ovk@group.calendar.google.com',
+                lazyFetching: true
+            }
+        ],
+    });
+    
+    calendar.setOption('locale', 'br');
+    calendar.render();
+});
